203509d797a5b8bc394d63c91cc5029fccd0d601
vagolf <vagolf123@gmail.com>
2025-08-10 11:49:43 +0700

    Success
    can add, save. It's success web

--- script.js
+++ script.js
@@ -10,11 +10,30 @@ function addTask() {
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
         let span = document.createElement("span");
-        span.innerHTML = "&#10006;";
-        span.onclick = function() {
-            listContainer.removeChild(li);
-        };
+        span.innerHTML = "\u00d7";
         li.appendChild(span);
     }
     inputBox.value = "";
+    saveData();
 }
+
+listContainer.addEventListener("click", function(event) {
+    if(event.target.tagName === "LI") {
+        event.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(event.target.tagName === "SPAN") {
+        event.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
